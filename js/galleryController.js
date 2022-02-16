38163fa81d3b1032b7d24311bcf1a72862391960
shemer binner <shemer146@gmail.com>
2022-02-16 21:25:19 +0200

    finished the header

--- js/galleryController.js
+++ js/galleryController.js
@@ -1,7 +1,8 @@
 'use strict'
 
 function onInit() {
-    renderGallery()
+    renderGallery();
+    renderSearchBar();
 }
 
 function renderGallery() {
@@ -21,4 +22,22 @@ function onImgSelect(imgId) {
 
     renderEditController()
     renderMeme()
+}
+
+function renderSearchBar() {
+    const strHtml = `
+    <div class="search-container">
+    <input class="search-input" id="search-input" type="search" placeholder="Enter search keyword">
+    <label for="search-input" class="search-label"><i class="fa-solid fa-magnifying-glass"></i></label>            
+    
+                </div>
+            <div class="key-words flex space-between grow">
+            <span>funny</span>
+            <span>dog</span>
+            <span>cat</span>
+            <span>baby</span>
+            <span class="search-more-btn">more...</span>
+            </div>`
+
+    document.querySelector('.search-bar').innerHTML = strHtml;
 }
